446a87364158b3d82ee287b8136c6dd9005f4d64
Snimso <115739027+Snimso@users.noreply.github.com>
2024-05-25 01:56:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+
+prototxt_path = 'colorization_deploy_v2.prototxt'
+model_path = 'colorization_release_v2.caffemodel'
+kernel_path = 'pts_in_hull.npy'
+image_path = 'testimages/mountain.jpg'
+
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+points = np.load(kernel_path)  #np clusters (kernels) needed for the model
+
+# populate cluster centers as 1x1 convolution kernel (src=https://github.com/opencv/opencv/blob/master/samples/dnn/colorization.py, opencv docs)
+points = points.transpose().reshape(2,313,1,1)
+net.getLayer(net.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]    #LAB -> LIGHTNESS, A, B 
+net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+bw_image = cv2.imread(image_path)
+normalized = bw_image.astype("float32") / 255.0  #normalize from rgb to lab max rgb val is 255
+lab = cv2.cvtColor(normalized, cv2.COLOR_BGR2LAB)
+
+#resize 224x224 (model trained for this )
+resized = cv2.resize(lab, (224,224))
+L = cv2.split(resized)[0]
+L-= 50
+
+net.setInput(cv2.dnn.blobFromImage(L))
+ab = net.forward()[0, :, :, :].transpose((1,2,0))
+
+ab = cv2.resize(ab, (bw_image.shape[1], bw_image.shape[0]))
+L = cv2.split(lab)[0] 
+
+colorized = np.concatenate((L[:, :, np.newaxis],ab), axis=2)
+colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+colorized = (255.0 * colorized).astype("uint8")
+
+cv2.imshow("Black And White Image", bw_image)
+cv2.imshow("Colorized Image", colorized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
